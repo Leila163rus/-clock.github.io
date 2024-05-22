b00a4b6823b2bbdc8e39ea4b651fabc95f2d3dbb
Лейла <151535513+Leila163rus@users.noreply.github.com>
2024-05-22 17:55:08 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,23 +4,17 @@ const date = document.querySelector('.date');
 const time = document.querySelector('.time');
 let week = ['Воскресенье', 'Понедельник', 'Вторник', 'Срeда', 'Четверг', 'Пятница', 'Суббота']
 function currentTime () {
-    let today = new Date();
-    let year = today.getFullYear();
-    let month = today.getMonth()+1
-    let day = today.getDate();
-    month = (month < 10) ? '0' + month:month;
-    day = (day < 10) ? '0' + day:day;
-    date.innerHTML = year.toString() + '-' + month.toString() + '-' + day.toString() + " " + week[today.getDay()];
+  let today = new Date();
+  let year = today.getFullYear();
+  let month = (today.getMonth()+1).toString().padStart(2, '0');
+  let day = today.getDate().toString().padStart(2, '0');
+  let weekDay = week[today.getDay()];
+  date.textContent = `${year}-${month}-${day} ${weekDay}`;
 
-    let hours = today.getHours();
-    let minutes = today.getMinutes();
-    let seconds = today.getSeconds();
-    hours = (hours < 10) ? '0' + hours:hours;
-    minutes = (minutes < 10) ? '0' + minutes:minutes;
-    seconds = (seconds < 10) ? '0' + seconds:seconds;
-    time.innerHTML = hours + ':' + minutes + ':' + seconds;
-    setTimeout(currentTime, 1000);
+  let hours = today.getHours().toString().padStart(2, '0');
+  let minutes = today.getMinutes().toString().padStart(2, '0');
+  let seconds = today.getSeconds().toString().padStart(2, '0');
+  time.textContent = `${hours}:${minutes}:${seconds}`;
+  setTimeout(currentTime, 1000);
 }
 currentTime();
-
-
