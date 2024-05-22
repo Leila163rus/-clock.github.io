200260e93d0e8117542e4287c0e585a88c4ceabd
Пользователь <lilu163@mail.ru>
2024-05-22 20:01:32 +0400

    all

---
+++ js/main.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const date = document.querySelector('.date');
+const time = document.querySelector('.time');
+let week = ['Воскресенье', 'Понедельник', 'Вторник', 'Срeда', 'Четверг', 'Пятница', 'Суббота']
+function currentTime () {
+  let today = new Date();
+  let year = today.getFullYear();
+  let month = (today.getMonth()+1).toString().padStart(2, '0');
+  let day = today.getDate().toString().padStart(2, '0');
+  let weekDay = week[today.getDay()];
+  date.textContent = `${year}-${month}-${day} ${weekDay}`;
+
+  let hours = today.getHours().toString().padStart(2, '0');
+  let minutes = today.getMinutes().toString().padStart(2, '0');
+  let seconds = today.getSeconds().toString().padStart(2, '0');
+  time.textContent = `${hours}:${minutes}:${seconds}`;
+  setTimeout(currentTime, 1000);
+}
+currentTime();
+
